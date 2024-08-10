ea35b694230f22a63822431894b30a5d9953a074
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-08-10 16:38:38 +0200

    format with prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/home';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Home from "./pages/home"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>
-);
-
-
+    <Home />
+  </React.StrictMode>,
+)
