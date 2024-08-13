44885044f6f19bd203df32e58b3006cfc9cea1dc
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-08-13 10:55:43 +0200

    add router

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Home from "./pages/home"
 import Header from "./components/header"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <Header />
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
