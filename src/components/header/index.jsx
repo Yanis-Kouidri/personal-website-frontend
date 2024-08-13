862fc4fff6f6ce29219f2f76023d1844fd2d8f7c
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-08-13 10:49:36 +0200

    chore: Add header component to the application

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <nav>
+      <div>Accueil</div>
+      <div>Projets</div>
+      <div>Documentations</div>
+      <div>À propos</div>
+    </nav>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Home from "./pages/home"
+import Header from "./components/header"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
+    <Header />
     <Home />
   </React.StrictMode>,
 )
