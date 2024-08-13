d05ba7093047d7ac6edf02c467aabb5c59431ffb
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-08-13 11:38:26 +0200

    add a styled nav bar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,16 +4,23 @@ import styled from "styled-components"
 const StyledLink = styled(Link)`
   padding: 15px;
   color: blue;
+  font-size: 20px;
+`
+
+const StyledNav = styled.nav`
+  padding: 15px;
+  display: flex;
+  justify-content: center;
 `
 
 function Header() {
   return (
-    <nav>
+    <StyledNav>
       <StyledLink to="/">Accueil</StyledLink>
       <StyledLink to="/projects">Projets</StyledLink>
       <StyledLink to="/docs">Documentations</StyledLink>
       <StyledLink to="/about">À propos</StyledLink>
-    </nav>
+    </StyledNav>
   )
 }
 
