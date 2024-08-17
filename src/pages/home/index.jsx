ba5078bffe8bfb4633dbd6092dfcdf6320a79602
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-08-17 11:48:42 +0200

    add style

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,16 @@
 import yanis from "../../assets/pictures/photo_accueil_yanis.jpg"
 import styled from "styled-components"
+import colors from "../../utils/style/colors"
 
 const FirstSection = styled.section`
   display: flex;
   justify-content: center;
+  background: linear-gradient(
+    to right,
+    ${colors.sixth} calc(50% + 220px),
+    ${colors.fourth} calc(0%)
+  );
+  padding: 100px;
 `
 
 const PresentationPicutre = styled.img`
@@ -13,18 +20,22 @@ const PresentationPicutre = styled.img`
 
 const StyledTitle = styled.h1`
   line-height: 50px;
-  font-size: 32px;
+  font-size: 42px;
 `
 
+const StyledTextPart = styled.div``
+
 function Home() {
-  const shortDescription = "Ingénieur en informatique à l'ENSEEIHT"
+  const shortDescription = "Ingénieur en informatique et réseaux à l'ENSEEIHT."
+  const location = "🌆 Toulouse, France."
 
   return (
     <FirstSection>
-      <div>
+      <StyledTextPart>
         <StyledTitle>Yanis Kouidri</StyledTitle>
         <p>{shortDescription}</p>
-      </div>
+        <p>{location}</p>
+      </StyledTextPart>
       <PresentationPicutre src={yanis} alt="Yanis Kouidri" />
     </FirstSection>
   )
--- src/utils/style/colors.js
+++ src/utils/style/colors.js
@@ -1,6 +1,10 @@
 const colors = {
-  primary: "#084DFC",
-  secondary: "#089AFC",
+  primary: "#0F1EF5",
+  secondary: "#0F67F5",
+  third: "#0FF5F1",
+  fourth: "#480FF5",
+  fith: "#36BCF5",
+  sixth: "#5F99F5",
   backgroundColor: "#eeeeee",
 }
 
