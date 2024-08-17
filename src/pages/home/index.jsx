cfae19797d65b4d536e2dca70a1ceda1e2cce68d
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-08-17 11:17:49 +0200

    add picture and style

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,32 @@
+import yanis from "../../assets/pictures/photo_accueil_yanis.jpg"
+import styled from "styled-components"
+
+const FirstSection = styled.section`
+  display: flex;
+  justify-content: center;
+`
+
+const PresentationPicutre = styled.img`
+  height: 300px;
+  padding-left: 50px;
+`
+
+const StyledTitle = styled.h1`
+  line-height: 50px;
+  font-size: 32px;
+`
+
 function Home() {
+  const shortDescription = "Ingénieur en informatique à l'ENSEEIHT"
+
   return (
-    <div>
-      <h1>Yanis Kouidri</h1>
-    </div>
+    <FirstSection>
+      <div>
+        <StyledTitle>Yanis Kouidri</StyledTitle>
+        <p>{shortDescription}</p>
+      </div>
+      <PresentationPicutre src={yanis} alt="Yanis Kouidri" />
+    </FirstSection>
   )
 }
 
