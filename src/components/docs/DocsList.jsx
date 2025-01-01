d0910aa3e6940b707ad82f68817cdbe846f34e3f
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-01-01 19:01:28 +0100

    add a loading buffer

--- src/components/docs/DocsList.jsx
+++ src/components/docs/DocsList.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react"
+import { Loader } from "../../utils/style/CommonStyles"
 
 function DocsList() {
   const [docs, setDocs] = useState([])
   const [errorMessage, setErrorMessage] = useState("")
+  const [isFetching, setIsFetching] = useState(true)
   const standardErrorMessage =
     "Ooops, il semble qu'il y ait une erreur coté serveur"
   const backendUrl = process.env.REACT_APP_BACKEND_URL
@@ -16,7 +18,6 @@ function DocsList() {
     fetch(backendUrl + "/api/docs")
       .then((response) => {
         if (!response.ok) {
-          setErrorMessage(standardErrorMessage)
           throw new Error("Connection impossible")
         }
         return response.text()
@@ -25,12 +26,14 @@ function DocsList() {
         try {
           const data = JSON.parse(text) // Try to parse JSON
           setDocs(data)
+          setIsFetching(false)
         } catch (error) {
-          setErrorMessage(standardErrorMessage)
           throw new SyntaxError("Error during JSON parsing: " + error.message)
         }
       })
       .catch((error) => {
+        setErrorMessage(standardErrorMessage)
+        setIsFetching(false)
         console.error("Error fetching docs:", error)
       })
   }, [backendUrl])
@@ -39,20 +42,24 @@ function DocsList() {
     <div>
       <h1>Liste des PDF</h1>
       {errorMessage && <p>{errorMessage}</p>}
-      <ul>
-        {docs &&
-          docs.map((doc, index) => (
-            <li key={index}>
-              <a
-                href={`${backendUrl}/data/docs/${doc}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {doc}
-              </a>
-            </li>
-          ))}
-      </ul>
+      {isFetching ? (
+        <Loader />
+      ) : (
+        <ul>
+          {docs &&
+            docs.map((doc, index) => (
+              <li key={index}>
+                <a
+                  href={`${backendUrl}/data/docs/${doc}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {doc}
+                </a>
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   )
 }
--- src/utils/style/CommonStyles.jsx
+++ src/utils/style/CommonStyles.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+import colors from './colors'
 
 export const BasicH2Title = styled.h2`
   font-size: 38px;
@@ -14,4 +15,26 @@ export const InBuildingImg = styled.img`
 
 export const InBuildingWrapper = styled.div`
   text-align: center;
+`
+
+
+ 
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+ 
+    to {
+    transform: rotate(360deg);
+    }
+`
+ 
+export const Loader = styled.div`
+    padding: 10px;
+    border: 6px solid ${colors.primary};
+    border-bottom-color: transparent;
+    border-radius: 22px;
+    animation: ${rotate} 1s infinite linear;
+    height: 0;
+    width: 0;
 `
