fdc665dc8767a9e4c6d6e662eda98d156dd71943
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-01-05 11:37:23 +0100

    add test post request for login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,7 @@
 import { BasicH2Title, BasicWrapper } from "../../utils/style/CommonStyles"
 import styled from "styled-components"
 import colors from "../../utils/style/colors"
+import { useState } from "react"
 
 const StyledForm = styled.form`
   display: flex;
@@ -29,21 +30,76 @@ const StyledSubmitButton = styled.button`
   }
 `
 
-function handleSubmit(e) {
-  e.preventDefault()
-}
-
 function Login() {
-  return (
-    <BasicWrapper>
-      <BasicH2Title>Connexion</BasicH2Title>
-      <StyledForm onSubmit={handleSubmit}>
-        <StyledInput name="username" type="text" placeholder="Username" />
-        <StyledInput name="password" type="password" placeholder="Password" />
-        <StyledSubmitButton type="submit">Se connecter</StyledSubmitButton>
-      </StyledForm>
-    </BasicWrapper>
-  )
-}
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const backendUrl = process.env.REACT_APP_BACKEND_URL
+  if (!backendUrl) {
+    console.log(
+      "REACT_APP_BACKEND_URL env variable is empty : connection impossible",
+    )
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const requestBody = {
+      username: username,
+      password: password,
+    }
+
+    try {
+      const response = await fetch(backendUrl + "/api/auth/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      })
+
+      if (response.status === 401) {
+        const errorData = await response.json()
+        setErrorMessage(errorData.message)
+      } else {
+        if (response.status === 200) {
+          const authData = await response.json()
+          console.log(authData)
+          //const token = authData.token
+          //const userId = authData.userId
+        } else {
+          throw new Error("Unexpected response from backend")
+        }
+      }
+    } catch (e) {
+      console.error("Server error :" + e)
+    }
+	}
+
+    return (
+      <BasicWrapper>
+        <BasicH2Title>Connexion</BasicH2Title>
+        {errorMessage && <p>{errorMessage}</p>}
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput
+            name="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <StyledInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <StyledSubmitButton type="submit">Se connecter</StyledSubmitButton>
+        </StyledForm>
+      </BasicWrapper>
+    )
+  }
 
 export default Login
