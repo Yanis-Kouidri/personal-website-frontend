3f6e09834a55f662b8fedd346f27fc365f50aeea
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-01-15 21:08:37 +0100

    recept token in login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,13 +4,18 @@ import {
   StyledForm,
   StyledInput,
   StyledSubmitButton,
+  StyledSuccessMessage,
+  StyledErrorMessage,
 } from "../../utils/style/CommonStyles"
 import { useState } from "react"
 
 function Login() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
-  const [errorMessage, setErrorMessage] = useState("") 
+  const [errorMessage, setErrorMessage] = useState("")
+  const [successMessage, setSuccessMessage] = useState("")
+
+  const [loading, setLoading] = useState(false)
 
   const backendUrl = process.env.REACT_APP_BACKEND_URL
   if (!backendUrl) {
@@ -21,6 +26,9 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setErrorMessage("")
+    setSuccessMessage("")
+    setLoading(true)
 
     const requestBody = {
       username: username,
@@ -34,31 +42,42 @@ function Login() {
           "content-type": "application/json",
         },
         body: JSON.stringify(requestBody),
+        credentials: "include",
       })
 
-      if (response.status === 401) {
-        const errorData = await response.json()
-        setErrorMessage(errorData.message)
-      } else {
-        if (response.status === 200) {
-          setErrorMessage("")
-          const authData = await response.json()
-          console.log(authData)
-          //const token = authData.token
-          //const userId = authData.userId
-        } else {
+      switch (response.status) {
+        case 200:
+          const responseData = await response.json()
+          setSuccessMessage(responseData.message)
+          break
+        case 401:
+          const errorData = await response.json()
+          setErrorMessage(errorData.message)
+          break
+        case 429:
+          setErrorMessage("Too many failures, plese wait a while")
+          break
+        case 500:
+          setErrorMessage("Internal server error")
+          break
+        default:
           throw new Error("Unexpected response from backend")
-        }
       }
     } catch (e) {
+      setErrorMessage("Internal server error")
       console.error("Server error :" + e)
+    } finally {
+      setLoading(false)
     }
   }
 
   return (
     <BasicWrapper>
       <BasicH2Title>Connexion</BasicH2Title>
-      {errorMessage && <p>{errorMessage}</p>}
+      {errorMessage && <StyledErrorMessage>{errorMessage}</StyledErrorMessage>}
+      {successMessage && (
+        <StyledSuccessMessage>{successMessage}</StyledSuccessMessage>
+      )}
       <StyledForm onSubmit={handleSubmit}>
         <StyledInput
           name="username"
@@ -74,7 +93,9 @@ function Login() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <StyledSubmitButton type="submit">Se connecter</StyledSubmitButton>
+        <StyledSubmitButton type="submit" disabled={loading}>
+          {loading ? "Chargement..." : "Se connecter"}
+        </StyledSubmitButton>
       </StyledForm>
     </BasicWrapper>
   )
