fe3b73729fb6d1d6bde8145872f4dcd9d17ce131
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-07-19 15:00:11 +0200

    remove unused import

--- src/components/docs/RecursiveList.jsx
+++ src/components/docs/RecursiveList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import config from '../../utils/config'
 import AddFileButton from './AddFileButton'
 import AddFolderButton from './AddFolderButton'
