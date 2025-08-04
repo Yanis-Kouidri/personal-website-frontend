75278e8c2dbf3ddd5b043f046ab67115bc0e77ba
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-08-04 08:35:19 +0200

    update: sri plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sri } from 'vite-plugin-sri3'
+import { sri } from 'vite-plugin-sri4'
 
 // https://vite.dev/config/
 export default defineConfig({
