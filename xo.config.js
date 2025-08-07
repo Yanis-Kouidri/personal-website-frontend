ce9b511474a314c590f33443f002acf366c4619e
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-08-07 22:12:40 +0200

    update: config

--- xo.config.js
+++ xo.config.js
@@ -1,8 +1,21 @@
 /** @type {import('xo').FlatXoConfig} */
-const xoConfig = [{
-  react: true,
-  prettier: 'compat',
-  space: true,
-}]
+const xoConfig = [
+  {
+    react: true,
+    prettier: 'compat',
+    space: true,
+    rules: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            kebabCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+    },
+  },
+]
 
 export default xoConfig
