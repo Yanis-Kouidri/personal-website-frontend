1f379884b3f75ceeb2c77a73dc72507e79173600
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-08-08 08:45:30 +0200

    use new eslint plugin

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import eslintJs from '@eslint/js'
+import eslintReact from '@eslint-react/eslint-plugin'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.js', '**/*.jsx'],
+
+    // Extend recommended rule sets from:
+    // 1. ESLint JS's recommended rules
+    // 2. ESLint React's recommended rules
+    extends: [eslintJs.configs.recommended, eslintReact.configs.recommended],
+
+    // Configure language/parsing options
+    languageOptions: {
+      // Include browser global variables (window, document, etc.)
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Enable JSX syntax support
+        },
+      },
+    },
+
+    // Custom rule overrides (modify rule levels or disable rules)
+    rules: {},
+  },
+])
